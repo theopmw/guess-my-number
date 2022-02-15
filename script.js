15e2b8eb00aab76a114985bcdc01ca5e0ad98094
theopmw <theowrightdev@gmail.com>
2022-02-15 18:35:53 +0000

    Add guess too high and guess too low logic

--- script.js
+++ script.js
@@ -11,5 +11,9 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '⛔️ No Number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too High!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too Low!';
   }
 });
