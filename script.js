dac6409cf70324a9f270a04f82048d59cdd9b906
theopmw <theowrightdev@gmail.com>
2022-02-15 18:32:27 +0000

    Add logic for correct number

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 'use strict';
 
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔️ No number!';
+    document.querySelector('.message').textContent = '⛔️ No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number';
   }
 });
