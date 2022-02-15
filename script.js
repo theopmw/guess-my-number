b7c14949bb0967df56104804612f6c5f989ed65d
theopmw <theowrightdev@gmail.com>
2022-02-15 18:40:17 +0000

    Add logic to decrease score with wrong guess

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -13,7 +14,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '🎉 Correct Number';
   } else if (guess > secretNumber) {
     document.querySelector('.message').textContent = '📈 Too High!';
+    score--;
+    document.querySelector('.score').textContent = score;
   } else if (guess < secretNumber) {
     document.querySelector('.message').textContent = '📉 Too Low!';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
 });
