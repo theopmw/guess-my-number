ab1e9b2b493b098a1f6eb882a2080eab7d8ad017
theopmw <theowrightdev@gmail.com>
2022-02-16 08:33:26 +0000

    Add Again! button functionality

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 'use strict';
 
 // Generate random number between 1 & 20
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 // Set score to 20
 let score = 20;
-
-// Set text content of secret number
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -22,6 +19,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
 
     // When guess is too high
   } else if (guess > secretNumber) {
@@ -46,4 +44,23 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  // Restore initial value of score variable
+  score = 20;
+  // Restore initial value of secretNumber variable
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  // Restore initial conditions of the message
+  document.querySelector('.message').textContent = 'Start guessing...';
+  // Restore initial conditions of the score
+  document.querySelector('.score').textContent = score;
+  // Restore initial conditions of the number
+  document.querySelector('.number').textContent = '?';
+  // Restore initial conditions of the input field
+  document.querySelector('.guess').value = '';
+  // Restore initial background color
+  document.querySelector('body').style.backgroundColor = '#222';
+  // Restore initial number element width
+  document.querySelector('.number').style.width = '15rem';
 });
