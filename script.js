39a895402c8a0cf8fb5ecdd052ada6170818371a
theopmw <theowrightdev@gmail.com>
2022-02-16 12:20:08 +0000

    Add highscore functionality

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 // Set score to 20
 let score = 20;
 
+// Set highscore to 0
+let highscore = 0;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -20,6 +23,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // When guess is too high
   } else if (guess > secretNumber) {
